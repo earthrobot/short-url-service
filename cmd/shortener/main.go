8965ab96b46e44a8930ded7ce72d5ed29f8803d5
earthrobot <syley-20@yandex.ru>
2023-10-16 17:29:21 +0000

    add tests

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -29,41 +29,50 @@ func (ds *DataStorage) Get(key string) (string, bool) {
 
 var dataStorage = NewDataStorage()
 
+func createShortLinkHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+		return
+	}
+
+	link := string(body)
+
+	hash := sha256.Sum256([]byte(link))
+	shortLink := fmt.Sprintf("%x", hash)
+
+	dataStorage.Set(shortLink, link)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusCreated)
+	returnLink := "http://localhost:8080/" + shortLink
+	w.Write([]byte(returnLink))
+}
+
+func getOriginalLinkHandler(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.Path, "/")
+	if len(parts) < 2 {
+		http.Error(w, "Invalid URL", http.StatusBadRequest)
+		return
+	}
+	shortLink := parts[1]
+	link, ok := dataStorage.Get(shortLink)
+	if !ok {
+		http.Error(w, "URL not found", http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Location", link)
+	w.WriteHeader(307)
+}
+
 func mainHandle(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method == http.MethodPost {
 
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Failed to read request body", http.StatusBadRequest)
-			return
-		}
-
-		link := string(body)
-
-		hash := sha256.Sum256([]byte(link))
-		shortLink := fmt.Sprintf("%x", hash)
-
-		dataStorage.Set(shortLink, link)
-		w.WriteHeader(http.StatusCreated)
-		returnLink := "http://localhost:8080/" + shortLink
-		w.Write([]byte(returnLink))
+		createShortLinkHandler(w, r)
 
 	} else if r.Method == http.MethodGet {
 
-		parts := strings.Split(r.URL.Path, "/")
-		if len(parts) < 2 {
-			http.Error(w, "Invalid URL", http.StatusBadRequest)
-			return
-		}
-		shortLink := parts[1]
-		link, ok := dataStorage.Get(shortLink)
-		if !ok {
-			http.Error(w, "URL not found", http.StatusBadRequest)
-			return
-		}
-		w.Header().Set("Location", link)
-		w.WriteHeader(307)
+		getOriginalLinkHandler(w, r)
 	}
 }
 
---
+++ cmd/shortener/main_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestCreateShortLinkHandler(t *testing.T) {
+	link := "https://ya.ru"
+	request, err := http.NewRequest("POST", "/", strings.NewReader(link))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	createShortLinkHandler(recorder, request)
+
+	if status := recorder.Code; status != http.StatusCreated {
+		t.Errorf("Handler returned wrong status code: got %v expected %v", status, http.StatusCreated)
+	}
+
+	contentType := recorder.Header().Get("Content-Type")
+	if contentType != "text/plain; charset=utf-8" {
+		t.Errorf("Handler returned wrong Content-Type header: got %v want %v", contentType, "text/plain; charset=utf-8")
+	}
+
+	if recorder.Body.Len() == 0 {
+		t.Errorf("Handler returned an empty response body, but expected some content")
+	}
+}
+
+func TestGetOriginalLinkHandler(t *testing.T) {
+	link := "https://ya.ru"
+	requestPost, err := http.NewRequest("POST", "/", strings.NewReader(link))
+	if err != nil {
+		t.Fatal(err)
+	}
+	recorderPost := httptest.NewRecorder()
+	createShortLinkHandler(recorderPost, requestPost)
+
+	shortLink := recorderPost.Body.String()
+	requestGet, err := http.NewRequest("GET", shortLink, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	recorderGet := httptest.NewRecorder()
+	getOriginalLinkHandler(recorderGet, requestGet)
+
+	if status := recorderGet.Code; status != 307 {
+		t.Errorf("Handler returned wrong status code: got %v expected %v", status, 307)
+	}
+
+	location := recorderGet.Header().Get("Location")
+	if location != link {
+		t.Errorf("Handler returned wrong Location header: got %v want %v", location, link)
+	}
+
+}
