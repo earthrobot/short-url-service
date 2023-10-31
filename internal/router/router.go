9632877b250b213794cbd36ee165775bd9972112
earthrobot <syley-20@yandex.ru>
2023-10-31 17:12:48 +0000

    add gzip

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"github.com/earthrobot/short-url-service/internal/encoding"
 	"github.com/earthrobot/short-url-service/internal/logger"
 	"github.com/earthrobot/short-url-service/internal/storage"
 	"github.com/go-chi/chi/v5"
@@ -18,6 +19,7 @@ func NewRouter() *Router {
 
 	logger.Initialize()
 	s.Router.Use(logger.RequestLogger)
+	s.Router.Use(encoding.GzipMiddleware)
 	s.Router.Post("/", h.createShortLinkHandler)
 	s.Router.Get("/{linkHash}", h.getOriginalLinkHandler)
 	s.Router.Post("/api/shorten", h.createShortLinkAPIHandler)
