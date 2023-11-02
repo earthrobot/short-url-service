b78c857dda7a0a22e02ae280d256f668e0660b74
earthrobot <syley-20@yandex.ru>
2023-11-02 18:44:35 +0000

    fix flags

--- config/config.go
+++ config/config.go
@@ -17,11 +17,11 @@ func Load() {
 	ConfSet.AppHost = getConfigValue("a", "localhost:8080", "SERVER_ADDRESS", "Host for the app")
 	ConfSet.URLHost = getConfigValue("b", "http://localhost:8080", "BASE_URL", "Host for links")
 	ConfSet.FileStoragePath = getConfigValue("f", "/tmp/short-url-db.json", "FILE_STORAGE_PATH", "File storage path")
+	flag.Parse()
 }
 
 func getConfigValue(flagName, defaultValue, envName, description string) string {
 	flagValue := flag.String(flagName, defaultValue, description)
-	flag.Parse()
 
 	if envValue := os.Getenv(envName); envValue != "" {
 		return envValue
