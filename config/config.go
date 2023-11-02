7e61d9aadb2d8b6717eb6b103cb76ddb696c06f2
earthrobot <syley-20@yandex.ru>
2023-11-02 19:13:24 +0000

    fix flags

--- config/config.go
+++ config/config.go
@@ -14,18 +14,20 @@ type Config struct {
 var ConfSet Config
 
 func Load() {
-	ConfSet.AppHost = getConfigValue("a", "localhost:8080", "SERVER_ADDRESS", "Host for the app")
-	ConfSet.URLHost = getConfigValue("b", "http://localhost:8080", "BASE_URL", "Host for links")
-	ConfSet.FileStoragePath = getConfigValue("f", "/tmp/short-url-db.json", "FILE_STORAGE_PATH", "File storage path")
+	fileStoragePath := flag.String("f", "/tmp/short-url-db.json", "File storage path")
+	appHost := flag.String("a", "localhost:8080", "Host for the app")
+	urlHost := flag.String("b", "http://localhost:8080", "Host for links")
+
 	flag.Parse()
+
+	ConfSet.FileStoragePath = getEnvOrDefault("FILE_STORAGE_PATH", *fileStoragePath)
+	ConfSet.AppHost = getEnvOrDefault("SERVER_ADDRESS", *appHost)
+	ConfSet.URLHost = getEnvOrDefault("BASE_URL", *urlHost)
 }
 
-func getConfigValue(flagName, defaultValue, envName, description string) string {
-	flagValue := flag.String(flagName, defaultValue, description)
-
+func getEnvOrDefault(envName, defaultValue string) string {
 	if envValue := os.Getenv(envName); envValue != "" {
 		return envValue
 	}
-
-	return *flagValue
+	return defaultValue
 }
